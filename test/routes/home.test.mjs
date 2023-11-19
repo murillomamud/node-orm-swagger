d3cbfb86212d50b0dd34d3fb2f01e1bba308b044
Murillo Mamud <murillomamud@gmail.com>
2023-11-19 11:12:33 -0300

    added test to home.mjs

---
+++ test/routes/home.test.mjs
@@ -0,0 +1,14 @@
+import { test, expect } from 'vitest';
+import request from 'supertest';
+import express from 'express';
+import homeRouter from '../../src/routes/home.mjs';
+
+test('homeRouter deve retornar Hello World!', async ({ is }) => {
+  const app = express();
+  app.get('/', homeRouter);
+
+  const response = await request(app).get('/');
+
+  expect(response.status).toBe(200);
+  expect(response.text).toBe('Hello World!');
+});
