074d5a556f9fe4c482e487b1a439cbabbce2338a
Murillo Mamud <murillomamud@gmail.com>
2023-11-20 12:17:11 -0300

    fix sg egress rule

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -22,7 +22,7 @@ resource "aws_security_group" "allow_http" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["::/0"]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
 }
