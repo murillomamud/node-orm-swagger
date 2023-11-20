4a86a0dd689ae53bf71d968bdad48d52698324d8
Murillo Mamud <murillomamud@gmail.com>
2023-11-20 13:12:24 -0300

    add task role

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -9,7 +9,7 @@ resource "aws_ecs_task_definition" "my_task_definition" {
   cpu = 256
   memory = 512
   execution_role_arn = aws_iam_role.ecs_execution_role.arn
-
+  task_role_arn = aws_iam_role.ecs_task_role.arn
   container_definitions = jsonencode([
     {
       name  = "container-product-api"
--- terraform/iam.tf
+++ terraform/iam.tf
@@ -35,7 +35,29 @@ resource "aws_iam_role" "ecs_execution_role" {
   })
 }
 
+resource "aws_iam_role" "ecs_task_role" {
+    name = "product-api-task-role"
+    
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17",
+        Statement = [
+        {
+            Effect    = "Allow",
+            Principal = {
+            Service = "ecs-tasks.amazonaws.com",
+            },
+            Action    = "sts:AssumeRole",
+        },
+        ],
+    })
+}
+
 resource "aws_iam_role_policy_attachment" "ecs_execution_policy_attachment" {
   role       = aws_iam_role.ecs_execution_role.name
   policy_arn = aws_iam_policy.ecs_execution_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_policy_attachment" {
+    role       = aws_iam_role.ecs_task_role.name
+    policy_arn = aws_iam_policy.ecs_execution_policy.arn
 }
