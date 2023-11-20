fa65ae0f9e9445c9a3172ef42bafdfe8a983fae0
Murillo Mamud <murillomamud@gmail.com>
2023-11-20 11:51:55 -0300

    add endpoint to logs

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -21,3 +21,15 @@ resource "aws_vpc_endpoint" "ecr_dkr" {
 
   private_dns_enabled = true
 }
+
+resource "aws_vpc_endpoint" "cloudwatch_logs" {
+  vpc_id            = data.aws_vpc.my_vpc.id
+  service_name      = "com.amazonaws.us-east-1.logs"
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [aws_security_group.allow_http.id]
+
+  subnet_ids = [data.aws_subnet.my_subnet.id]
+
+  private_dns_enabled = true
+}
