70ee2746416faae7c3ea418022d3d5e89ee7a757
Murillo Mamud <murillomamud@gmail.com>
2023-11-20 11:18:12 -0300

    add egress

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -16,5 +16,26 @@ resource "aws_security_group" "allow_http" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Permitir tráfego na porta 3000 para todos os IPs externos
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Permitir tráfego na porta 3000 para todos os IPs externos
+  }  
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Permitir tráfego na porta 3000 para todos os IPs externos
   }  
 }
